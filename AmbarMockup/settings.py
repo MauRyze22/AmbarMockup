75f5ca4715caa63b2acd89f4bb87fc2f07e89218
MauRyze22 <amaurymperez22@gmail.com>
2026-09-04 00:24:56 -0400

    Correcion en settings

--- AmbarMockup/settings.py
+++ AmbarMockup/settings.py
@@ -80,11 +80,20 @@ WSGI_APPLICATION = 'AmbarMockup.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 
-
-DATABASES={
-        'default': dj_database_url.config(default=config("DATABASE_URL"))
+DATABASE_URL = config('DATABASE_URL', default=None)
+ 
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(default=DATABASE_URL)
     }
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+ 
 
 
 # Password validation
